deff20f14fec519f7e71836aa5b6fb7623da1bb7
Shakena <shakenacme@gmail.com>
2019-07-01 11:04:00 -0400

    Replacing comments with my own comments

--- js/script.js
+++ js/script.js
@@ -3,30 +3,8 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
 
-
-/***
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
-
-// QUOTES:
-    // 1. "Whether you think you can or you think you can't, you're right." - Henry Ford
-    // 2. "Humility is not thinking less of yourself, it's thinking of //yourself less." - C.S. Lewis
-    // 3. "It is never too late to be what you might have been." - George Eliot
-    // 4. "To live a creative life, we must lose our fear of being wrong." - Anonymous
-    // 5. "Unless you try to do something beyond what you have already mastered, you will never grow." - Ralph Waldo Emerson
-    // 6. "The most common way people give up their power is by thinking they don't have any." - Alice Walker
-    // 7. "I attribute my success to this: I never gave or took any excuse." - Florence Nightingale
-    // 8. "Everything you've ever wanted is on the other side of fear." - George Addair
-    // 9. "Under who's rules are you living? Under who's wishes are you living? For who are you living." - Kyo, Dir en grey
-    // 10. "Every great developer you know got there by solving problems they were unqualified to solve until they actually did it." - Patrick McKenzie
-
+// Array of objects called 'quotes' that stores the properties: quote, source, citation, and year
 let quotes = [
   {
     quote: "I attribute my success to this: I never gave or took any excuse.",
@@ -53,31 +31,20 @@ let quotes = [
 ];
 
 
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number
-   - Cse the random number to `return` a random quote object from the `quotes` array.
-***/
-
+// Create getRandomQuote() function to create a random number
+  // Store the random number in the 'randomNumber' variable
+  // return that random quote using the randomNumber variable
 function getRandomQuote() {
   let randomNumber = Math.floor(Math.random() * quotes.length);
    return quotes[randomNumber];
 };
 
 
-/***
-  Create the `printQuote` function to:
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string.
-***/
-
+// Create printQuote() function to generate and print a random quote
+  // Store the getRandomQuote() function into a variable 'randomQuote'
+  // Generate a random quote along with the source and store it in the string 'html'
+  // Use if statements to add the source and citation to the html string if applicable
+  // Use the getElementById() method and innerHTML property to change the HTML content of the <p> element with the 'quote-box' id
 function printQuote() {
    let randomQuote = getRandomQuote();
    let html = '';
@@ -95,14 +62,9 @@ function printQuote() {
 }
 
 
-/***
+/*
   When the "Show another quote" button is clicked, the event listener
   below will be triggered, and it will call, or "invoke", the `printQuote`
-  function. So do not make any changes to the line of code below this
-  comment.
-***/
-
+  function.
+*/
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
