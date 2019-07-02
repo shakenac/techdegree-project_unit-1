134458adf3fd932df676586a57f4f168ec982ac9
Shakena <shakenacme@gmail.com>
2019-07-02 10:01:10 -0400

    Revised comments

--- js/script.js
+++ js/script.js
@@ -12,7 +12,6 @@ let quotes = [
     citation: "The Life of Florence Nightengale",
     year: "1913 (1861)",
     tags: "#inspirational"
-    // quoteinvestigator.com/2016/07/30/excuse/
   },
   {
     quote: "Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.",
@@ -27,7 +26,6 @@ let quotes = [
     citation: "The Reader’s Digest",
     year: "1947",
     tags: "#inspirational"
-    // quoteinvestigator.com/2015/02/03/you-can/
   },
   {
     quote: "Everything you've ever wanted is on the other side of fear.",
@@ -40,9 +38,9 @@ let quotes = [
     citation: "Forbes: Thoughts on the Business of Life",
     year: "1945",
     tags: "#inspirational, #business"
-    // quoteinvestigator.com/2015/02/24/grow/#note-10698-1
   }
 ];
+
 
 // Create getRandomQuote() function to create a random number
   // Store the random number in the 'randomNumber' variable
@@ -52,15 +50,16 @@ function getRandomQuote() {
    return quotes[randomNumber];
 };
 
+// Function to generate random background color
 function getRandomColor() {
-  // Randomly generate number for the RGB color string
+  // randomly generate numbers for the RGB color string
   let red = Math.floor(Math.random() * 256 );
   let green = Math.floor(Math.random() * 256 );
   let blue = Math.floor(Math.random() * 256 );
   let rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
 
   // this getElementsByTagName method returns all the elements with the body tag
-  // the getElementsByTagName gives you an array like object, [0] returns the first (and only) body tag
+  // the getElementsByTagName gives you an array like object; [0] returns the first (and only) body tag
   // added the rgbColor variable to change the css to a random color
   document.getElementsByTagName('body')[0].style.background = rgbColor;
 
@@ -68,21 +67,25 @@ function getRandomColor() {
   document.getElementsByTagName('button')[0].style.background = rgbColor;
 }
 
-// QuoteTimer() function automatically generates a new quote every 10 seconds
+
+// quoteTimer() function automatically generates a new quote every 20 seconds
 function quoteTimer() {
-  setInterval(printQuote, 10000);
+  setInterval(printQuote, 20000);
 }
+
 
 // Create printQuote() function to generate and print a random quote
 function printQuote() {
+
    // stored the getRandomQuote() function into a variable 'randomQuote'
    let randomQuote = getRandomQuote();
-   // Generate a random quote along with the source and store it in the string 'html'
+
+   // generate a random quote along with the source and store it in the string 'html'
    let html = '';
    html += '<p class="quote">' + randomQuote.quote + '</p>';
    html += '<p class="source">' + randomQuote.source;
 
-   // Use if statements to add the source and citation to the html string if applicable
+   // use if statements to add the source and citation to the html string if applicable
    if (randomQuote.citation) {
       html += '<span class="citation">' + randomQuote.citation + '</span>';
    }
@@ -100,11 +103,11 @@ function printQuote() {
    // Use the getElementById() method and innerHTML property to change the HTML content of the <p> element with the 'quote-box' id
    document.getElementById('quote-box').innerHTML = html;
 
-   // call getRandomColor function to generate new color each time button is pressed
+   // call the getRandomColor function to generate new color each time button is pressed
    getRandomColor();
 }
 
-// call quoteTimer function to generate new quote every 10 seconds
+// call the quoteTimer function to generate new quote every 20 seconds
 quoteTimer()
 
 
